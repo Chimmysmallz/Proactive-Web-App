25b6cf62c0a388e3f5c9ae1c1a338c2e14f6a498
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2023-03-01 12:54:34 +0100

    Create footer.php

---
+++ web/php/footer.php
@@ -0,0 +1,60 @@
+<footer>
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-lg-4 col-md-6">
+
+                <h4>About Us</h4>
+
+                <p>We are a project management tool that helps you manage your projects more efficiently.</p>
+
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+
+                <h4>Quick Links</h4>
+
+                <ul class="list-unstyled">
+
+                    <li><a href="index.php">Home</a></li>
+
+                    <li><a href="project-list.php">Projects</a></li>
+
+                    <li><a href="contact.php">Contact</a></li>
+
+                </ul>
+
+            </div>
+
+            <div class="col-lg-4 col-md-12">
+
+                <h4>Contact Us</h4>
+
+                <p><strong>Phone:</strong> 555-123-4567</p>
+
+                <p><strong>Email:</strong> info@proactive.com</p>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="container-fluid bg-dark">
+
+        <div class="row">
+
+            <div class="col-lg-12">
+
+                <p class="text-center">&copy; 2023 Proactive. All Rights Reserved.</p>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</footer>
+
