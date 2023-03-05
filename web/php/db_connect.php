33311e0b2c7c853bd856dcc40d9ef4f23fdae8d8
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2023-03-05 18:31:04 +0100

    Update db_connect.php

--- web/php/db_connect.php
+++ web/php/db_connect.php
@@ -1,5 +1,8 @@
 <?php
 
+// include queries.php
+require_once 'queries.php';
+
 // Set database credentials
 
 $db_host = "localhost";
@@ -18,9 +21,21 @@ $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
 if (!$conn) {
 
-    die("Connection failed: " . mysqli_connect_error());
-
+    if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
 }
 
-?>
+// example usage of SQL queries
+$email = 'example@example.com';
+$stmt = $mysqli->prepare(QUERY_GET_USER_BY_EMAIL);
+$stmt->bind_param('s', $email);
+$stmt->execute();
+$result = $stmt->get_result();
+$user = $result->fetch_assoc();
 
+$name = 'Example Project';
+$user_id = $user['id'];
+$project_id = insert_project($name, $user_id);
+
+?>
