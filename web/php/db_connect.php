bc93d5b7ee619e8f56098f920f74af21d78dd96b
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2023-03-09 20:25:07 +0100

    Update db_connect.php

--- web/php/db_connect.php
+++ web/php/db_connect.php
@@ -1,29 +1,18 @@
 <?php
 
-// include queries.php
-require_once 'queries.php';
-
 // Set database credentials
 
 $db_host = "localhost";
-
 $db_user = "proactive_user";
-
 $db_pass = "password";
-
 $db_name = "proactive";
 
 // Establish database connection
-
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
 // Check for connection errors
-
 if (!$conn) {
-
-    if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
-    exit();
+    die("Connection failed: " . mysqli_connect_error());
 }
 
 function checkUserLogin($email, $password) {
@@ -59,7 +48,7 @@ function checkUserLogin($email, $password) {
 
 // example usage of SQL queries
 $email = 'example@example.com';
-$stmt = $mysqli->prepare(QUERY_GET_USER_BY_EMAIL);
+$stmt = $conn->prepare(QUERY_GET_USER_BY_EMAIL);
 $stmt->bind_param('s', $email);
 $stmt->execute();
 $result = $stmt->get_result();
