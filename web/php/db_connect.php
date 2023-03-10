cc388e90a916be2325d632d4292265c0f5c3cdfd
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2023-03-10 15:20:33 +0100

    Update db_connect.php

--- web/php/db_connect.php
+++ web/php/db_connect.php
@@ -1,7 +1,5 @@
 <?php
-
 // Set database credentials
-
 $db_host = "localhost";
 $db_user = "proactive_user";
 $db_pass = "password";
@@ -18,97 +16,81 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+// Display PHP errors
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Define SQL query
 define('QUERY_GET_USER_BY_EMAIL', 'SELECT id FROM Users WHERE email = ?');
 
+// Define functions
 function checkUserLogin($email, $password) {
-  global $conn;
-  // prepare SQL statement
-  $stmt = $conn->prepare("SELECT id FROM Users WHERE email = ? AND password = ?");
-
-  // check for errors preparing the statement
-  if (!$stmt) {
-    die("Error preparing statement: " . $conn->error);
-  }
-
-  // bind parameters
-  $stmt->bind_param("ss", $email, $password);
-
-  // execute statement
-  $stmt->execute();
-
-  // check for errors executing the statement
-  if ($stmt->error) {
-    die("Error executing statement: " . $stmt->error);
-  }
-
-  // bind result variables
-  $stmt->bind_result($id);
-
-  // fetch value
-  $stmt->fetch();
-
-  // close statement and connection
-  $stmt->close();
-
-  // return user id or false if no user found
-  if ($id) {
-    return $id;
-  } else {
-    return false;
-  }
+    global $conn;
+    // prepare SQL statement
+    $stmt = $conn->prepare("SELECT id FROM Users WHERE email = ? AND password = ?");
+    // check for errors preparing the statement
+    if (!$stmt) {
+        die("Error preparing statement: " . $conn->error);
+    }
+    // bind parameters
+    $stmt->bind_param("ss", $email, $password);
+    // execute statement
+    $stmt->execute();
+    // check for errors executing the statement
+    if ($stmt->error) {
+        die("Error executing statement: " . $stmt->error);
+    }
+    // bind result variables
+    $stmt->bind_result($id);
+    // fetch value
+    $stmt->fetch();
+    // close statement and connection
+    $stmt->close();
+    // return user id or false if no user found
+    if ($id) {
+        return $id;
+    } else {
+        return false;
+    }
 }
 
-
 function insert_project($name, $user_id) {
-  global $conn;
-
-  // prepare SQL statement
-  $stmt = $conn->prepare("INSERT INTO Projects (name, user_id) VALUES (?, ?)");
-
-  // check for errors preparing the statement
-  if (!$stmt) {
-    die("Error preparing statement: " . $conn->error);
-  }
-
-  // bind parameters
-  $stmt->bind_param("si", $name, $user_id);
-
-  // execute statement
-  $stmt->execute();
-
-  // get inserted project ID
-  $project_id = $stmt->insert_id;
-
-  // close statement
-  $stmt->close();
-
-  return $project_id;
+    global $conn;
+    // prepare SQL statement
+    $stmt = $conn->prepare("INSERT INTO Projects (name, user_id) VALUES (?, ?)");
+    // check for errors preparing the statement
+    if (!$stmt) {
+        die("Error preparing statement: " . $conn->error);
+    }
+    // bind parameters
+    $stmt->bind_param("si", $name, $user_id);
+    // execute statement
+    $stmt->execute();
+    // get inserted project ID
+    $project_id = $stmt->insert_id;
+    // close statement
+    $stmt->close();
+    return $project_id;
 }
 
 // example usage of SQL queries
 $email = 'example@example.com';
 $stmt = $conn->prepare(QUERY_GET_USER_BY_EMAIL);
-
 // check for errors preparing the statement
 if (!$stmt) {
-  die("Error preparing statement: " . $conn->error);
+    die("Error preparing statement: " . $conn->error);
 }
-
 $stmt->bind_param('s', $email);
-
 // execute statement
 $stmt->execute();
-
 // check for errors executing the statement
 if ($stmt->error) {
-  die("Error executing statement: " . $stmt->error);
+    die("Error executing statement: " . $stmt->error);
 }
-
 $result = $stmt->get_result();
 $user = $result->fetch_assoc();
-
 $name = 'Example Project';
 $user_id = $user['id'];
 $project_id = insert_project($name, $user_id);
-
 ?>
